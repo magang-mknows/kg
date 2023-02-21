db8a0693e82bfc76b00f3932f0cc741b23f7f5f9
Maulana Sodiqin <maulanasdqn@gmail.com>
2023-02-21 16:55:29 +0700

    feature: text field

--- src/components/Common/TextField.tsx
+++ src/components/Common/TextField.tsx
@@ -1,20 +1,21 @@
 import { FC, Fragment, ReactElement } from "react";
+import { TextFieldProps } from "./types";
 
-const TextField: FC = (): ReactElement => {
+const TextField: FC<TextFieldProps> = ({ label, required, type, name, placeholder }): ReactElement => {
   return (
     <Fragment>
       <label
-        htmlFor="email"
+        htmlFor={name}
         className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
       >
-        Your email
+        {label}
       </label>
       <input
-        type="email"
-        name="email"
+        type={type}
+        name={name}
         className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        placeholder="name@company.com"
-        required
+        placeholder={placeholder}
+        required={required}
       />
     </Fragment>
   );
