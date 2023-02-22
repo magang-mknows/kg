58b082272ac25f45629afbd252cef8984b52db30
mochilhamsyah17 <mochilhamsyah17@gmail.com>
2023-02-22 15:19:39 +0700

    bugfixing: moving screens in tailwind configs from theme to extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    screens: {
-      xs: '240px',
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px'
+  theme: {},
+    extend: {
+      screens: {
+        xs: '240px',
+        sm: '480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1440px'
+      },
     },
-    extend: {},
-  },
   plugins: [],
 };
