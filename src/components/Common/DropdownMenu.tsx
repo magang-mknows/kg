f2eb08db6f9970f39983e3d3aad811bc8e4a7670
Fenny Oktaviani <fnny04@gmail.com>
2023-02-23 16:36:43 +0700

    feature: build card

--- src/components/Common/DropdownMenu.tsx
+++ src/components/Common/DropdownMenu.tsx
@@ -4,7 +4,7 @@ import { DropdownProps } from "./types";
 
 const DropdownMenu: FC<DropdownProps> = ({ children, list }): ReactElement => {
   return (
-    <Menu as="div" className="relative inline-block text-left">
+    <Menu as="div" className=" relative inline-block text-left">
       {children}
       <Transition
         as={Fragment}
@@ -15,7 +15,7 @@ const DropdownMenu: FC<DropdownProps> = ({ children, list }): ReactElement => {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute right-0  mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <Menu.Items className="absolute right-0 z-20  mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
           {list.map((x, i) => (
             <div key={i} className="px-1 py-1 ">
               <Menu.Item>
@@ -23,7 +23,7 @@ const DropdownMenu: FC<DropdownProps> = ({ children, list }): ReactElement => {
                   <button
                     className={`${
                       active ? "bg-violet-500 text-white" : "text-gray-900"
-                    } group flex w-full items-center rounded-md px-2 py-2 text-sm gap-x-4`}
+                    } group flex w-auto items-center rounded-md px-2 py-2 text-sm gap-x-4`}
                   >
                     {x.icon}
                     <span className="text-base">{x.text}</span>
