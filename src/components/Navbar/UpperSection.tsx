a3dd0f14f42683c5cfee1dc280944e118ba4acab
Mario Rudy Silalahi <115483667+bangmar@users.noreply.github.com>
2023-02-26 21:52:15 +0700

    bugfix : navbar logo

--- src/components/Navbar/UpperSection.tsx
+++ src/components/Navbar/UpperSection.tsx
@@ -26,7 +26,7 @@ const UpperSection: FC = () => {
         {/* kg logo's*/}
         <Link href={"/"} passHref className="cursor-pointer">
           <Image
-            src={theme === "light" ? logoKG : systemTheme === "light" ? logoKG : logoKGDark}
+            src={theme === "light" ? logoKG : systemTheme === "dark" ? logoKGDark : logoKG}
             alt="Kampus Gratis Logo's"
             className="transition-all opacity-100 ease-in-out duration-200 w-[5em] md:w-[7em] lg:w-[8em] "
           />
