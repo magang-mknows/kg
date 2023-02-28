8fa48ba9cdd5bef3d85ed61f584f7830512e1d0f
MARIO SIHALOHO <20102018@ittelkom-pwt.ac.id>
2023-03-01 01:25:01 +0700

    feature:new navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "",
+  darkMode: "class",
   theme: {
     primary: {
       100: "#CDF5FA",
