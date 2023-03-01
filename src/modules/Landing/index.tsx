9c25888a688425af389731a2fa3664b1f3bfedfe
GarnesPratama <97274527+GarnesPratama@users.noreply.github.com>
2023-03-01 14:34:10 +0700

    fix layout

--- src/modules/Landing/index.tsx
+++ src/modules/Landing/index.tsx
@@ -3,13 +3,14 @@ import BaseLayouts from "@/layouts/Base";
 
 const HeroSection = lazy(() => import("@/modules/Landing/HeroSection"));
 const AboutSection = lazy(() => import("@/modules/Landing/AboutSection"));
+const SekilasSection = lazy(() => import("@/modules/Landing/SekilasSection"));
 
 const Landing: FC = (): ReactElement => {
   return (
     <BaseLayouts>
       <Suspense fallback={"Skeleton loading...."}>
         <HeroSection />
-        <AboutSection />
+        <SekilasSection />
       </Suspense>
     </BaseLayouts>
   );
