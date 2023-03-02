8f24b58f30c614b1ff0af47016c74a7149b62a80
GarnesPratama <97274527+GarnesPratama@users.noreply.github.com>
2023-03-02 16:10:42 +0700

    switch index location to landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import { ReactElement } from "react";
-import Landing from "@/modules/Assigment";
+import Landing from "@/modules/Landing";
 
 
 const Index: NextPage = (): ReactElement => {
