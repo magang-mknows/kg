4aef0179cf635377af0ab4a8d83b606f6dfecfc7
Nandi <nandiirawan327@gmail.com>
2023-03-02 19:27:17 +0700

    feature: add testimony section in landing page

--- src/modules/Landing/index.tsx
+++ src/modules/Landing/index.tsx
@@ -5,12 +5,9 @@ const HeroSection = lazy(() => import("@/modules/Landing/HeroSection"));
 const MitraSection = lazy(() => import("@/modules/Landing/MitraSection"));
 const AboutSection = lazy(() => import("@/modules/Landing/AboutSection"));
 const SekilasSection = lazy(() => import("@/modules/Landing/SekilasSection"));
-<<<<<<< HEAD
 const TestimonySection = lazy(() => import("./TestimonySection"));
-=======
 const InformationBanner = lazy(() => import("@/modules/Landing/InformationBanner"));
 const PromotionBanner = lazy(() => import("@/modules/Landing/PromotionBanner"));
->>>>>>> 78c70dcf986b80c2ad36f8da6c7d1bc21c2f0b52
 
 const Landing: FC = (): ReactElement => {
   return (
@@ -19,13 +16,10 @@ const Landing: FC = (): ReactElement => {
         <HeroSection />
         <MitraSection />
         <SekilasSection />
-<<<<<<< HEAD
         <TestimonySection />
-=======
         <AboutSection />
         <InformationBanner />
         <PromotionBanner />
->>>>>>> 78c70dcf986b80c2ad36f8da6c7d1bc21c2f0b52
       </Suspense>
     </BaseLayouts>
   );
