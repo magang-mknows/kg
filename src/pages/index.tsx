4c8c037cb0956027023c713c7bef0e94c36fe9c0
GarnesPratama <97274527+GarnesPratama@users.noreply.github.com>
2023-03-03 10:47:21 +0700

    improvement : Create Assigment Pages Done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import { ReactElement } from "react";
-import Landing from "@/modules/Landing";
+import Landing from "@/modules/Assigment";
 
 const Index: NextPage = (): ReactElement => {
   return <Landing />;
