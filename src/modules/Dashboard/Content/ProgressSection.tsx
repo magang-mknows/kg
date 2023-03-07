ace2abb902928427e01d5bbd25b2708a00635b6d
Salsabila Dita Prasetya <avocadobanana33@gmail.com>
2023-03-07 16:55:45 +0700

    add button and its logic

--- src/modules/Dashboard/Content/ProgressSection.tsx
+++ src/modules/Dashboard/Content/ProgressSection.tsx
@@ -10,7 +10,7 @@ const montserrat = Montserrat({
 });
 
 const ProgressSection: FC = (): ReactElement => {
-  var percent = Math.floor(14/14*100);
+  let percent = Math.floor(14/14*100);
   return (
     <section
       className={`${montserrat.className} mx-4 my-4 bg-[#ffffff] w-full pt-[28px] px-[24px]`}
