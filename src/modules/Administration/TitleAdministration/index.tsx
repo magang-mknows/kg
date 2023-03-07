12866adda8235e74e063761cb41ed30353fdfadf
GarnesPratama <97274527+GarnesPratama@users.noreply.github.com>
2023-03-07 15:57:23 +0700

    improvement : create stepper administration pages

---
+++ src/modules/Administration/TitleAdministration/index.tsx
@@ -0,0 +1,12 @@
+import MainLayouts from "@/layouts/Main";
+import React from "react";
+
+const TitleAdministration = () => {
+  return (
+    <div className="title px-[70px] w-full">
+      <h1 className="text-[20px] font-[600] ml-2">Administrasi</h1>
+    </div>
+  );
+};
+
+export default TitleAdministration;
