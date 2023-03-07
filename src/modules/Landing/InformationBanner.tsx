7a4982d55cbcf026a80696d0ef8f7074b10f655c
mochilhamsyah17 <mochilhamsyah17@gmail.com>
2023-03-07 16:51:32 +0700

    update: font weight in hero section

--- src/modules/Landing/InformationBanner.tsx
+++ src/modules/Landing/InformationBanner.tsx
@@ -11,11 +11,17 @@ const InformationBanner: FC = (): ReactElement => {
   return (
     <Card className="" img={InfoLanding} imgWidth={"w-full"} imgHeight={"h-full"}>
       <div className="grid lg:leading-[54px] gap-[14px] lg:gap-[28px] p-[32px] lg:px-[64px] place-content-center relative">
-        <h1 className="text-[22px] lg:text-[44px] text-white font-semibold text-center lg:text-start">
+        <h1 className="text-[22px] lg:text-[44px] text-white font-bold text-center lg:text-start">
           Temukan Kemudahan Belajar dengan Model Pembelajaran Fleksibel yang Inovatif
         </h1>
-        <GlobalButton color="green" size="regular" to="/" text={"Lihat Semua"} />
-        <Image className="absolute bottom-0 right-0 hidden lg:block" src={CardDot} alt="card-dot" />
+        <GlobalButton
+          color="green"
+          size="regular"
+          to="/"
+          text={"Lihat Semua"}
+          className="justify-center"
+        />
+        <Image className="absolute bottom-0 right-0 hidden " src={CardDot} alt="card-dot" />
       </div>
     </Card>
   );
