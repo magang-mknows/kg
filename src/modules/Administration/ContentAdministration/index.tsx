ba11121b3d5e604a7801d46e34d9b9a039a2a69b
GarnesPratama <97274527+GarnesPratama@users.noreply.github.com>
2023-03-07 12:02:59 +0700

    improvement : organize folder accordion in content

--- src/modules/Administration/ContentAdministration/index.tsx
+++ src/modules/Administration/ContentAdministration/index.tsx
@@ -1,7 +1,15 @@
+import MainLayouts from "@/layouts/Main";
 import React, { ReactElement } from "react";
+import JobsInformation from "../JobsInformation";
+import PrivateInformationAccordion from "../PrivateInformationAccordion";
 
 const index = (): ReactElement => {
-  return <div>index</div>;
+  return (
+    <MainLayouts>
+      <JobsInformation />
+      <PrivateInformationAccordion />
+    </MainLayouts>
+  );
 };
 
 export default index;
