61a4462cc7d0ea82e8315597027e507e00ece509
Noor Yulita Apsari <72492009+lita15@users.noreply.github.com>
2023-03-07 12:43:43 +0700

    impovement: add breadcrumb administration

--- src/modules/Administration/index.tsx
+++ src/modules/Administration/index.tsx
@@ -1,5 +1,8 @@
 import BaseLayouts from "@/layouts/Base";
 import React, { FC, lazy, ReactElement, Suspense } from "react";
+import { administrationBreadCumbs } from "@/utilities/constant";
+import BreadCrumb from "@/components/Assigment/BreadCrumb";
+import MainLayouts from "@/layouts/Main";
 
 const ContentAdministration = lazy(() => import("@/modules/Administration/ContentAdministration"));
 
@@ -7,6 +10,11 @@ const Administration: FC = (): ReactElement => {
   return (
     <BaseLayouts>
       <Suspense fallback={"skeleton loading..."}>
+        <BreadCrumb items={administrationBreadCumbs} />
+        <MainLayouts>
+          <h1 className="text-[20px] font-[600] ml-2">Administrasi</h1>
+        </MainLayouts>
+
         <ContentAdministration />
       </Suspense>
     </BaseLayouts>
