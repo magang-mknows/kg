a9e18f9402a4a7e34f5ea8dab139b68c295c79ae
mochilhamsyah17 <mochilhamsyah17@gmail.com>
2023-03-10 15:14:45 +0700

    improvement: update pop up menu for change profile photo

--- src/modules/Profile/EditProfileSection.tsx
+++ src/modules/Profile/EditProfileSection.tsx
@@ -1,7 +1,8 @@
 import GlobalButton from "@/components/Common/GlobalButton";
-import { FC, ReactElement, Suspense } from "react";
+import { FC, ReactElement, Suspense, Fragment, useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import SelectField from "@/components/Common/SelectField";
+import { Menu, Transition } from "@headlessui/react";
 
 // asset
 import userProfileImg from "@/assets/profile-user-img.svg";
@@ -47,13 +48,54 @@ const EditProfile: FC = (): ReactElement => {
                         className="w-[100px] h-[100px] z-20"
                       ></Image>
 
-                      <button className="absolute ml-[66px] top-[96px] bottom-[4px] rounded-full w-[32px] flex justify-center bg-yellow-100 h-[32px] z-50">
-                        <Image
-                          src={camera}
-                          alt="camera"
-                          className="w-[15px] h-[11px] mt-[10px]"
-                        ></Image>
-                      </button>
+                      <Menu as="div" className="relative inline-block text-left">
+                        <div>
+                          <Menu.Button className="absolute ml-[66px] top-[-40px] bottom-[4px] rounded-full w-[32px] flex justify-center bg-yellow-100 h-[32px] z-50">
+                            <Image
+                              src={camera}
+                              alt="camera"
+                              className="w-[15px] h-[11px] mt-[10px]"
+                            ></Image>
+                          </Menu.Button>
+                        </div>
+                        <Transition
+                          as={Fragment}
+                          enter="transition ease-out duration-100"
+                          enterFrom="transform opacity-0 scale-95"
+                          enterTo="transform opacity-100 scale-100"
+                          leave="transition ease-in duration-75"
+                          leaveFrom="transform opacity-100 scale-100"
+                          leaveTo="transform opacity-0 scale-95"
+                        >
+                          <Menu.Items className="absolute left-20 w-40 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                            <div className="px-1 py-1 ">
+                              <Menu.Item>
+                                {({ active }) => (
+                                  <button
+                                    className={`${
+                                      active ? "bg-neutral-100 text-gray-900" : "text-gray-900"
+                                    } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                                  >
+                                    Ambil Foto
+                                  </button>
+                                )}
+                              </Menu.Item>
+                              <Menu.Item>
+                                {({ active }) => (
+                                  <button
+                                    className={`${
+                                      active ? "bg-neutral-100 text-gray-900 " : "text-gray-900"
+                                    } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                                  >
+                                    Unggah Foto
+                                  </button>
+                                )}
+                              </Menu.Item>
+                            </div>
+                          </Menu.Items>
+                        </Transition>
+                      </Menu>
+
                       <div className="my-2">
                         <p className="font-semibold text-lg">Bandi Aljabar</p>
                         <p className="text-[#737373] px-4">Mahasiswa</p>
