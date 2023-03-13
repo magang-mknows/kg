0905312d64ea2ccb860b4084170f027eb2b44be1
GarnesPratama <97274527+GarnesPratama@users.noreply.github.com>
2023-03-13 16:32:43 +0700

    improvement : add atom certificate

--- src/stores/Score/index.tsx
+++ src/stores/Score/index.tsx
@@ -1,8 +1,21 @@
+import { type } from "os";
 import { atom } from "recoil";
+import { certificateTypes } from "./type";
 
 export const ShowDetailStatus = atom({
   key: "ShowDetailStatus",
   default: false,
 });
 
-
+export const CertificateStatus = atom<Array<certificateTypes>>({
+  key: "certificate-status",
+  default: [
+    {
+      id: 1,
+      name: "tes",
+      role: "Webinar",
+      type: "webinar",
+      urlDownload: "",
+    },
+  ],
+});
