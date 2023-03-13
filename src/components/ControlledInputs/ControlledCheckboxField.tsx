9589eac8464aed3e6bd86b7d3aa48fded347fbd1
Maulana Sodiqin <maulanasdqn@gmail.com>
2023-03-13 19:40:04 +0700

    improvement: simplified controlled inputs

--- src/components/ControlledInputs/ControlledCheckboxField.tsx
+++ src/components/ControlledInputs/ControlledCheckboxField.tsx
@@ -1,37 +1,14 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { ReactElement } from "react";
-import { Control, Controller } from "react-hook-form";
+import { FC, ReactElement } from "react";
+import { useController } from "react-hook-form";
 import CheckboxField from "../Common/CheckboxField";
 import { CheckboxFieldProps } from "../Common/types";
 
-type ControlledCheckboxFieldProps<T> = CheckboxFieldProps & {
-  control: Control<any, T>;
-  options?: { label: string; value: string | number | boolean }[];
-  name: string;
-  required?: boolean;
-};
-
-const ControlledCheckboxField = <T,>({
-  label,
-  control,
-  name,
-  ...rest
-}: ControlledCheckboxFieldProps<T>): ReactElement => {
-  return (
-    <Controller
-      control={control}
-      name={name}
-      render={({ field, fieldState: { error } }) => (
-        <CheckboxField
-          {...field}
-          {...rest}
-          label={label}
-          error={error?.message}
-          value={(field.value as unknown as never) || undefined}
-        />
-      )}
-    />
-  );
+const ControlledCheckboxField: FC<CheckboxFieldProps> = (props): ReactElement => {
+  const {
+    field,
+    fieldState: { error },
+  } = useController(props);
+  return <CheckboxField {...props} {...field} error={error?.message} />;
 };
 
 export default ControlledCheckboxField;
--- src/components/ControlledInputs/ControlledSelectField.tsx
+++ src/components/ControlledInputs/ControlledSelectField.tsx
@@ -1,38 +1,15 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { ReactElement } from "react";
-import { Control, Controller } from "react-hook-form";
+import { FC, ReactElement } from "react";
+import { useController } from "react-hook-form";
 import SelectField from "../Common/SelectField";
 import { SelectFieldProps } from "../Common/types";
 
-type ControlledSelectFieldProps<T> = SelectFieldProps & {
-  control: Control<any, T>;
-
-  options?: { label: string; value: string | number | boolean }[];
-  name: string;
-  mode?: "multiple" | "tags";
-  value?: string;
-  required?: boolean;
-};
-
-const ControlledSelectField = <T,>({
-  control,
-  name,
-  ...rest
-}: ControlledSelectFieldProps<T>): ReactElement => {
-  return (
-    <Controller
-      control={control}
-      name={name}
-      render={({ field, fieldState: { error } }) => (
-        <SelectField
-          {...field}
-          {...rest}
-          error={error?.message}
-          value={(field.value as unknown as never) || undefined}
-        />
-      )}
-    />
-  );
+const ControlledSelectField: FC<SelectFieldProps> = (props): ReactElement => {
+  const {
+    field,
+    fieldState: { error },
+  } = useController(props);
+  return <SelectField {...props} {...field} error={error?.message} />;
 };
 
 export default ControlledSelectField;
