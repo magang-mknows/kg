900039a9cd271bd2bd0231ec5941e75f0fac2823
GarnesPratama <97274527+GarnesPratama@users.noreply.github.com>
2023-03-14 16:31:20 +0700

    improvement : add dark mode in certificate and fix filter button

--- src/modules/Score/CertificateSection/index.tsx
+++ src/modules/Score/CertificateSection/index.tsx
@@ -9,6 +9,7 @@ import download from "@/assets/download.svg";
 import { useCertficate } from "@/hooks/Score";
 import { certificateTypes } from "@/stores/Score/type";
 import Link from "next/link";
+import { RxSlider } from "react-icons/rx";
 
 const CertificateSection = (): ReactElement => {
   const [open, setOpen] = useState(false);
@@ -17,57 +18,56 @@ const CertificateSection = (): ReactElement => {
   const { getCertificate } = useCertficate();
   return (
     <div className="w-full">
-      <div className="flex justify-end container">
+      <div className="flex justify-end  ">
         <button
-          className="w-[109px] h-[40px] rounded-[8px] text-center border-[3px] border-[#106FA4]"
+          className="w-[109px] h-[40px] rounded-[8px] text-center border-[3px] border-[#106FA4] relative "
           onClick={() => setOpen(open == true ? false : true)}
         >
-          <div className="my-auto flex justify-center gap-[10px]">
-            <p className="text-[#106FA4] text-[14px]">Filter</p>
-            <Image src={Filter} alt={"filter-icon"} />
+          <div className="my-auto flex justify-center gap-[10px] items-center">
+            <p className="text-[#106FA4] text-[14px] dark:text-white">Filter</p>
+            <RxSlider className="text-[#106FA4] dark:text-white" />
           </div>
+          {open && (
+            <div className="flex justify-end container mt-[10px] z-[9000] absolute w-[150px] -bottom-32 ease-in-out ">
+              <div className="w-[160px] h-[114px] rounded-[8px] text-center  overflow-hidden shadow-2xl ">
+                <button
+                  className="flex justify-start h-[38px] relative hover:bg-[#F5F5F5] w-full bg-white "
+                  onClick={() => setFilter("semua")}
+                >
+                  <div
+                    className={`h-full w-[5px] ${
+                      filter == "semua" ? "bg-[#67A5C8]" : "bg-[#FFFF]"
+                    } absolute left-0 overflow-hidden hover:hover:bg-[#F5F5F5]`}
+                  ></div>
+                  <p className="text-[#262626] text-[14px] my-auto ml-3">Semua</p>
+                </button>
+                <button
+                  className="flex justify-start h-[38px] relative hover:bg-[#F5F5F5] w-full bg-white "
+                  onClick={() => setFilter("mata-kuliah")}
+                >
+                  <div
+                    className={`h-full w-[5px] ${
+                      filter == "mata-kuliah" ? "bg-[#67A5C8]" : "bg-[#FFFF]"
+                    } absolute left-0 overflow-hidden hover:hover:bg-[#F5F5F5]`}
+                  ></div>
+                  <p className="text-[#262626] text-[14px] my-auto ml-3">Mata Kuliah</p>
+                </button>
+                <button
+                  className="flex justify-start h-[38px] relative hover:bg-[#F5F5F5] w-full bg-white "
+                  onClick={() => setFilter("webinar")}
+                >
+                  <div
+                    className={`h-full w-[5px] ${
+                      filter == "webinar" ? "bg-[#67A5C8]" : "bg-[#FFFF]"
+                    } absolute left-0 overflow-hidden`}
+                  ></div>
+                  <p className="text-[#262626] text-[14px] my-auto ml-3">Webinar</p>
+                </button>
+              </div>
+            </div>
+          )}
         </button>
       </div>
-
-      {open && (
-        <div className="flex justify-end container mt-[10px] z-[9000] absolute right-7 ease-in-out ">
-          <div className="w-[160px] h-[114px] rounded-[8px] text-center  overflow-hidden shadow-2xl ">
-            <button
-              className="flex justify-start h-[38px] relative hover:bg-[#F5F5F5] w-full bg-white "
-              onClick={() => setFilter("semua")}
-            >
-              <div
-                className={`h-full w-[5px] ${
-                  filter == "semua" ? "bg-[#67A5C8]" : "bg-[#FFFF]"
-                } absolute left-0 overflow-hidden hover:hover:bg-[#F5F5F5]`}
-              ></div>
-              <p className="text-[#262626] text-[14px] my-auto ml-3">Semua</p>
-            </button>
-            <button
-              className="flex justify-start h-[38px] relative hover:bg-[#F5F5F5] w-full bg-white "
-              onClick={() => setFilter("mata-kuliah")}
-            >
-              <div
-                className={`h-full w-[5px] ${
-                  filter == "mata-kuliah" ? "bg-[#67A5C8]" : "bg-[#FFFF]"
-                } absolute left-0 overflow-hidden hover:hover:bg-[#F5F5F5]`}
-              ></div>
-              <p className="text-[#262626] text-[14px] my-auto ml-3">Mata Kuliah</p>
-            </button>
-            <button
-              className="flex justify-start h-[38px] relative hover:bg-[#F5F5F5] w-full bg-white "
-              onClick={() => setFilter("webinar")}
-            >
-              <div
-                className={`h-full w-[5px] ${
-                  filter == "webinar" ? "bg-[#67A5C8]" : "bg-[#FFFF]"
-                } absolute left-0 overflow-hidden`}
-              ></div>
-              <p className="text-[#262626] text-[14px] my-auto ml-3">Webinar</p>
-            </button>
-          </div>
-        </div>
-      )}
 
       <div className=" lg:grid grid-cols-12 gap-[36px] relative mt-4 ">
         {filter == "semua"
@@ -78,15 +78,15 @@ const CertificateSection = (): ReactElement => {
                     hasImage={true}
                     src={dummyCertificate}
                     className={
-                      "container mx-auto w-full h-full  rounded-[8px] p-[12px] bg-white mb-10 lg:mb-0 "
+                      "container mx-auto w-full h-full  rounded-[8px] p-[12px] bg-white dark:bg-[#08405e] "
                     }
                     onClick={() => setPopupStatus(true)}
                   >
                     <div className="w-full mt-[12px]">
-                      <h1 className="text-center text-[#171717] text-[18px] font-[500]">
+                      <h1 className="text-center text-[#171717] text-[18px] font-[500] dark:text-white">
                         {item.name}
                       </h1>
-                      <p className="text-center text-[#A3A3A3] text-[16px] font-[500]">
+                      <p className="text-center text-[#A3A3A3] dark:text-white text-[16px] font-[500]">
                         {item.type}
                       </p>
                     </div>
@@ -124,7 +124,7 @@ const CertificateSection = (): ReactElement => {
               );
             })
           : getCertificate
-              .filter((item: certificateTypes) => item.role.includes(filter))
+              .filter((item: certificateTypes) => item.type.includes(filter))
               .map((item: certificateTypes) => {
                 return (
                   <div className="lg:col-span-4 w-full h-full mb-10 lg:mb-0 " key={item.id}>
@@ -132,15 +132,15 @@ const CertificateSection = (): ReactElement => {
                       hasImage={true}
                       src={dummyCertificate}
                       className={
-                        "container mx-auto w-full h-full  rounded-[8px] p-[12px] bg-white "
+                        "container mx-auto w-full h-full  rounded-[8px] p-[12px] bg-white dark:bg-[#08405e] "
                       }
                       onClick={() => setPopupStatus(true)}
                     >
                       <div className="w-full mt-[12px]">
-                        <h1 className="text-center text-[#171717] text-[18px] font-[500]">
+                        <h1 className="text-center text-[#171717] text-[18px] font-[500] dark:text-white">
                           {item.name}
                         </h1>
-                        <p className="text-center text-[#A3A3A3] text-[16px] font-[500]">
+                        <p className="text-center text-[#A3A3A3] text-[16px] font-[500] dark:text-white">
                           {item.type}
                         </p>
                       </div>
