dbeb0d0a9b08b9935b964fb3f95261f686135478
GarnesPratama <97274527+GarnesPratama@users.noreply.github.com>
2023-03-14 15:26:58 +0700

    improvement: add dark mode in score section

--- src/modules/Score/ScoreSection/SummaryTable.tsx
+++ src/modules/Score/ScoreSection/SummaryTable.tsx
@@ -10,7 +10,7 @@ const SummaryTable: FC = (): ReactElement => {
     <div className="flex flex-col">
       <div className="-m-1.5 overflow-x-auto mb-6">
         <div className="p-1.5 min-w-full inline-block align-middle">
-          <div className="border rounded-lg shadow overflow-hidden dark:border-gray-700 dark:shadow-gray-900">
+          <div className="border rounded-lg shadow overflow-hidden dark:border-white dark:shadow-gray-900">
             <table className="min-w-full divide-y divide-neutral-400 dark:divide-gray-700">
               <thead className="bg-primary-500">
                 <tr className="divide-x divide-neutral-300 dark:divide-gray-700">
@@ -33,28 +33,52 @@ const SummaryTable: FC = (): ReactElement => {
               </thead>
               <tbody className="divide-y divide-neutral-400 dark:divide-gray-700 text-center text-primary-600 font-bold">
                 <tr>
-                  <td className="px-6 py-4 border-r whitespace-nowrap text-sm ">3</td>
-                  <td className="px-6 py-4 border-r whitespace-nowrap text-sm ">108</td>
-                  <td className="px-6 py-4 border-r whitespace-nowrap text-sm ">96</td>
-                  <td className="px-6 py-4  border-r whitespace-nowrap text-sm ">4.00</td>
+                  <td className="px-6 py-4 border-r dark:border-r-white whitespace-nowrap text-sm dark:text-white ">
+                    3
+                  </td>
+                  <td className="px-6 py-4 border-r dark:border-r-white whitespace-nowrap text-sm dark:text-white ">
+                    108
+                  </td>
+                  <td className="px-6 py-4 border-r dark:border-r-white whitespace-nowrap text-sm  dark:text-white">
+                    96
+                  </td>
+                  <td className="px-6 py-4  border-r dark:border-r-white whitespace-nowrap text-sm dark:text-white">
+                    4.00
+                  </td>
                 </tr>
                 <tr>
-                  <td className="px-6 py-4 border-r whitespace-nowrap text-sm ">3</td>
-                  <td className="px-6 py-4 border-r whitespace-nowrap text-sm ">108</td>
-                  <td className="px-6 py-4 border-r whitespace-nowrap text-sm ">96</td>
-                  <td className="px-6 py-4  border-r whitespace-nowrap text-sm ">4.00</td>
+                  <td className="px-6 py-4 border-r dark:border-r-white whitespace-nowrap text-sm dark:text-white">
+                    3
+                  </td>
+                  <td className="px-6 py-4 border-r dark:border-r-white whitespace-nowrap text-sm dark:text-white">
+                    108
+                  </td>
+                  <td className="px-6 py-4 border-r dark:border-r-white whitespace-nowrap text-sm dark:text-white">
+                    96
+                  </td>
+                  <td className="px-6 py-4  border-r dark:border-r-white whitespace-nowrap text-sm dark:text-white">
+                    4.00
+                  </td>
                 </tr>
                 <tr>
-                  <td className="px-6 py-4 border-r whitespace-nowrap text-sm ">3</td>
-                  <td className="px-6 py-4 border-r whitespace-nowrap text-sm ">108</td>
-                  <td className="px-6 py-4 border-r whitespace-nowrap text-sm ">96</td>
-                  <td className="px-6 py-4  border-r whitespace-nowrap text-sm ">4.00</td>
+                  <td className="px-6 py-4 border-r dark:border-r-white whitespace-nowrap text-sm dark:text-white">
+                    3
+                  </td>
+                  <td className="px-6 py-4 border-r dark:border-r-white whitespace-nowrap text-sm dark:text-white">
+                    108
+                  </td>
+                  <td className="px-6 py-4 border-r dark:border-r-white whitespace-nowrap text-sm dark:text-white">
+                    96
+                  </td>
+                  <td className="px-6 py-4  border-r dark:border-r-white whitespace-nowrap text-sm dark:text-white">
+                    4.00
+                  </td>
                 </tr>
                 <tr>
-                  <td scope="col" className="text-centerpy-4" colSpan={3}>
+                  <td scope="col" className="text-centerpy-4 dark:text-white" colSpan={3}>
                     Rata-rata
                   </td>
-                  <td className="col-span-4 py-4">4.00</td>
+                  <td className="col-span-4 py-4 dark:text-white">4.00</td>
                 </tr>
               </tbody>
             </table>
@@ -70,7 +94,7 @@ const SummaryTable: FC = (): ReactElement => {
         <GlobalButton
           text="Lihat Detail Nilai"
           size="regular"
-          className=" bg-green-500 hover:bg-green-600 cursor-pointer transition-colors ease-in-out duration-300 shadow-md text-sm flex relative items-center justify-center text-white"
+          className=" bg-green-500 hover:bg-green-600 cursor-pointer transition-colors ease-in-out duration-300 shadow-md text-sm flex relative items-center justify-center text-white dark:bg-primary-500"
         />
       </div>
     </div>
