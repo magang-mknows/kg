b23f46a14b6f309b9c20bc6d25f742563326a3ff
Salsabila Dita Prasetya <avocadobanana33@gmail.com>
2023-03-14 18:10:28 +0700

    improvement: add icon, add enter-key, try filter(err)

--- src/modules/AllFeatures/Features.tsx
+++ src/modules/AllFeatures/Features.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactElement } from "react";
+import { FC, ReactElement, useState } from "react";
 import Card from "@/components/Common/Card";
 import icon1 from "@/assets/semuaFitur/icon1.svg";
 import icon2 from "@/assets/semuaFitur/icon2.svg";
@@ -12,48 +12,108 @@ import icon9 from "@/assets/semuaFitur/icon9.svg";
 import icon10 from "@/assets/semuaFitur/icon10.svg";
 import icon11 from "@/assets/semuaFitur/icon11.svg";
 import icon12 from "@/assets/semuaFitur/icon12.svg";
+import search from "@/assets/semuaFitur/search.svg";
+import Image from "next/image";
 import { StaticImageData } from "next/image";
 
-const DataFitur: { id: number; namaFitur: string; icon: StaticImageData; warna: string; slug: string }[] = [
-  { id: 1, namaFitur: "Administrasi", icon: icon1, warna: "#E9F6FD", slug:"/administrasi" },
+const DataFitur: {
+  id: number;
+  namaFitur: string;
+  icon: StaticImageData;
+  warna: string;
+  slug: string;
+}[] = [
+  { id: 1, namaFitur: "Administrasi", icon: icon1, warna: "#E9F6FD", slug: "/administrasi" },
   { id: 2, namaFitur: "Rencana Studi", icon: icon2, warna: "#FEF6D0", slug: "/rencana-studi" },
-  { id: 3, namaFitur: "Studi Ku", icon: icon1, warna: "#FEDBD7", slug: "/studiku"  },
-  { id: 4, namaFitur: "Nilai dan Sertifikat", icon: icon3, warna: "#E3FBDA", slug: "/nilai-sertifikat"  },
-  { id: 5, namaFitur: "Penugasan", icon: icon4, warna: "#FEDBD7", slug: "/penugasan"  },
-  { id: 6, namaFitur: "Diskusi Global", icon: icon5, warna: "#E3FBDA", slug: "/diskusi-global"  },
-  { id: 7, namaFitur: "Konsultasi & Layanan", icon: icon2, warna: "#FEF6D0", slug: "/konsultasi-dan-layanan"  },
-  { id: 8, namaFitur: "Simulasi, Drill & Assessmen", icon: icon6, warna: "#E9F6FD", slug: "/drill-simulasi"  },
-  { id: 9, namaFitur: "Kalender Saya", icon: icon1, warna: "#E9F6FD", slug: "/kalender-saya"  },
-  { id: 10, namaFitur: "Papan Skor", icon: icon7, warna: "#FEF6D0", slug: "/papan-skor"  },
-  { id: 11, namaFitur: "Analitik", icon: icon8, warna: "#FEDBD7", slug: "/analitik"  },
-  { id: 12, namaFitur: "Panduan", icon: icon9, warna: "#E3FBDA", slug: "/panduan"  },
-  { id: 13, namaFitur: "Sekilas Ilmu", icon: icon10, warna: "#FEDBD7", slug: "/sekilas-ilmu"  },
-  { id: 14, namaFitur: "Acara Kampus", icon: icon2, warna: "#E9F6FD", slug: "/acara-kampus"  },
-  { id: 15, namaFitur: "Perencanaan Karir", icon: icon11, warna: "#E3FBDA", slug: "/perencanaan-karir"  },
-  { id: 16, namaFitur: "Penyaluran Kerja", icon: icon12, warna: "#FEF6D0", slug: "/penyaluran-kerja"  },
+  { id: 3, namaFitur: "Studi Ku", icon: icon1, warna: "#FEDBD7", slug: "/studiku" },
+  {
+    id: 4,
+    namaFitur: "Nilai dan Sertifikat",
+    icon: icon3,
+    warna: "#E3FBDA",
+    slug: "/nilai-sertifikat",
+  },
+  { id: 5, namaFitur: "Penugasan", icon: icon4, warna: "#FEDBD7", slug: "/penugasan" },
+  { id: 6, namaFitur: "Diskusi Global", icon: icon5, warna: "#E3FBDA", slug: "/diskusi-global" },
+  {
+    id: 7,
+    namaFitur: "Konsultasi & Layanan",
+    icon: icon2,
+    warna: "#FEF6D0",
+    slug: "/konsultasi-dan-layanan",
+  },
+  {
+    id: 8,
+    namaFitur: "Simulasi, Drill & Assessmen",
+    icon: icon6,
+    warna: "#E9F6FD",
+    slug: "/drill-simulasi",
+  },
+  { id: 9, namaFitur: "Kalender Saya", icon: icon1, warna: "#E9F6FD", slug: "/kalender-saya" },
+  { id: 10, namaFitur: "Papan Skor", icon: icon7, warna: "#FEF6D0", slug: "/papan-skor" },
+  { id: 11, namaFitur: "Analitik", icon: icon8, warna: "#FEDBD7", slug: "/analitik" },
+  { id: 12, namaFitur: "Panduan", icon: icon9, warna: "#E3FBDA", slug: "/panduan" },
+  { id: 13, namaFitur: "Sekilas Ilmu", icon: icon10, warna: "#FEDBD7", slug: "/sekilas-ilmu" },
+  { id: 14, namaFitur: "Acara Kampus", icon: icon2, warna: "#E9F6FD", slug: "/acara-kampus" },
+  {
+    id: 15,
+    namaFitur: "Perencanaan Karir",
+    icon: icon11,
+    warna: "#E3FBDA",
+    slug: "/perencanaan-karir",
+  },
+  {
+    id: 16,
+    namaFitur: "Penyaluran Kerja",
+    icon: icon12,
+    warna: "#FEF6D0",
+    slug: "/penyaluran-kerja",
+  },
 ];
 
 const Features: FC = (): ReactElement => {
+  //ini buat di filter tp masi error jd blm dilanjutin
+  const [fitur, setFitur] = useState(DataFitur);
   return (
     <>
-      <section className="pt-5 px-[72px] bg-white ">
+      <section className="pt-5 px-[72px] bg-white w-full">
         <p className="font-bold text-[28px] text-center">Apa yang ingin kamu pelajari?</p>
         <p className="text-[#A3A3A3] font-semibold text-[16px] text-center">
           Kembangkan keterampilan Kamu dengan kursus dan sertifikasi online di Kampus Gratis.
           Berbagai macam jenis materi tersedia.
         </p>
-        <input
-          type="text"
-          className="bg-[#F5F5F5] my-[36px] rounded-lg py-[16px] px-[26px] w-[1146px]"
-          placeholder="Cari Fitur"
-        />
+        <div className="flex bg-[#F5F5F5] my-[36px] rounded-lg py-[16px] px-[26px] w-[1146px]">
+          <Image src={search} alt={"tes"} />
+          <input
+            type="text"
+            placeholder="Cari Fitur"
+            className="ml-[24px] w-full bg-[#F5F5F5] focus:outline-none"
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                console.log("berhasil beb");
+              }
+            }}
+          />
+        </div>
+
         <div className="grid grid-cols-12 gap-x-[40px] gap-y-[34px]">
-          {DataFitur.map((fitur) => {
+          {fitur.map((fitur) => {
+            // fitur.filter("Administrasi")
             return (
-              <div  key={fitur.id} style={{background: fitur.warna}} className="lg:col-span-3 md:col-span-4 col-span-6 py-[25px] rounded-lg">
-              <Card href={fitur.slug} hasImage={true} src={fitur.icon} imgStyle="w-[80px] mx-auto">
-                <p className="font-bold mx-auto">{fitur.namaFitur}</p>
-              </Card></div>
+              <div
+                key={fitur.id}
+                style={{ background: fitur.warna }}
+                className="lg:col-span-3 md:col-span-4 col-span-6 py-[25px] rounded-lg"
+              >
+                <Card
+                  href={fitur.slug}
+                  hasImage={true}
+                  src={fitur.icon}
+                  imgStyle="w-[80px] mx-auto"
+                >
+                  <p className="font-bold mx-auto">{fitur.namaFitur}</p>
+                </Card>
+              </div>
             );
           })}
         </div>
