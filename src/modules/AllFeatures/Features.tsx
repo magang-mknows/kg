d030205463e3def45cd72169c1496155d2d43c03
mochilhamsyah17 <mochilhamsyah17@gmail.com>
2023-03-14 17:28:51 +0700

    fixing: landing page and dark mode

--- src/modules/AllFeatures/Features.tsx
+++ src/modules/AllFeatures/Features.tsx
@@ -12,25 +12,61 @@ import icon9 from "@/assets/semuaFitur/icon9.svg";
 import icon10 from "@/assets/semuaFitur/icon10.svg";
 import icon11 from "@/assets/semuaFitur/icon11.svg";
 import icon12 from "@/assets/semuaFitur/icon12.svg";
-import Image, { StaticImageData } from "next/image";
+import { StaticImageData } from "next/image";
 
-const DataFitur: { id: number; namaFitur: string; icon: StaticImageData; warna: string; slug: string }[] = [
-  { id: 1, namaFitur: "Administrasi", icon: icon1, warna: "#E9F6FD", slug:"/administrasi" },
+const DataFitur: {
+  id: number;
+  namaFitur: string;
+  icon: StaticImageData;
+  warna: string;
+  slug: string;
+}[] = [
+  { id: 1, namaFitur: "Administrasi", icon: icon1, warna: "#E9F6FD", slug: "/administrasi" },
   { id: 2, namaFitur: "Rencana Studi", icon: icon2, warna: "#FEF6D0", slug: "/rencana-studi" },
-  { id: 3, namaFitur: "Studi Ku", icon: icon1, warna: "#FEDBD7", slug: "/studiku"  },
-  { id: 4, namaFitur: "Nilai dan Sertifikat", icon: icon3, warna: "#E3FBDA", slug: "/nilai-sertifikat"  },
-  { id: 5, namaFitur: "Penugasan", icon: icon4, warna: "#FEDBD7", slug: "/penugasan"  },
-  { id: 6, namaFitur: "Diskusi Global", icon: icon5, warna: "#E3FBDA", slug: "/diskusi-global"  },
-  { id: 7, namaFitur: "Konsultasi & Layanan", icon: icon2, warna: "#FEF6D0", slug: "/konsultasi-dan-layanan"  },
-  { id: 8, namaFitur: "Simulasi, Drill & Assessmen", icon: icon6, warna: "#E9F6FD", slug: "/drill-simulasi"  },
-  { id: 9, namaFitur: "Kalender Saya", icon: icon1, warna: "#E9F6FD", slug: "/kalender-saya"  },
-  { id: 10, namaFitur: "Papan Skor", icon: icon7, warna: "#FEF6D0", slug: "/papan-skor"  },
-  { id: 11, namaFitur: "Analitik", icon: icon8, warna: "#FEDBD7", slug: "/analitik"  },
-  { id: 12, namaFitur: "Panduan", icon: icon9, warna: "#E3FBDA", slug: "/panduan"  },
-  { id: 13, namaFitur: "Sekilas Ilmu", icon: icon10, warna: "#FEDBD7", slug: "/sekilas-ilmu"  },
-  { id: 14, namaFitur: "Acara Kampus", icon: icon2, warna: "#E9F6FD", slug: "/acara-kampus"  },
-  { id: 15, namaFitur: "Perencanaan Karir", icon: icon11, warna: "#E3FBDA", slug: "/perencanaan-karir"  },
-  { id: 16, namaFitur: "Penyaluran Kerja", icon: icon12, warna: "#FEF6D0", slug: "/penyaluran-kerja"  },
+  { id: 3, namaFitur: "Studi Ku", icon: icon1, warna: "#FEDBD7", slug: "/studiku" },
+  {
+    id: 4,
+    namaFitur: "Nilai dan Sertifikat",
+    icon: icon3,
+    warna: "#E3FBDA",
+    slug: "/nilai-sertifikat",
+  },
+  { id: 5, namaFitur: "Penugasan", icon: icon4, warna: "#FEDBD7", slug: "/penugasan" },
+  { id: 6, namaFitur: "Diskusi Global", icon: icon5, warna: "#E3FBDA", slug: "/diskusi-global" },
+  {
+    id: 7,
+    namaFitur: "Konsultasi & Layanan",
+    icon: icon2,
+    warna: "#FEF6D0",
+    slug: "/konsultasi-dan-layanan",
+  },
+  {
+    id: 8,
+    namaFitur: "Simulasi, Drill & Assessmen",
+    icon: icon6,
+    warna: "#E9F6FD",
+    slug: "/drill-simulasi",
+  },
+  { id: 9, namaFitur: "Kalender Saya", icon: icon1, warna: "#E9F6FD", slug: "/kalender-saya" },
+  { id: 10, namaFitur: "Papan Skor", icon: icon7, warna: "#FEF6D0", slug: "/papan-skor" },
+  { id: 11, namaFitur: "Analitik", icon: icon8, warna: "#FEDBD7", slug: "/analitik" },
+  { id: 12, namaFitur: "Panduan", icon: icon9, warna: "#E3FBDA", slug: "/panduan" },
+  { id: 13, namaFitur: "Sekilas Ilmu", icon: icon10, warna: "#FEDBD7", slug: "/sekilas-ilmu" },
+  { id: 14, namaFitur: "Acara Kampus", icon: icon2, warna: "#E9F6FD", slug: "/acara-kampus" },
+  {
+    id: 15,
+    namaFitur: "Perencanaan Karir",
+    icon: icon11,
+    warna: "#E3FBDA",
+    slug: "/perencanaan-karir",
+  },
+  {
+    id: 16,
+    namaFitur: "Penyaluran Kerja",
+    icon: icon12,
+    warna: "#FEF6D0",
+    slug: "/penyaluran-kerja",
+  },
 ];
 
 const Features: FC = (): ReactElement => {
@@ -50,10 +86,20 @@ const Features: FC = (): ReactElement => {
         <div className="grid grid-cols-12 gap-x-[40px] gap-y-[34px]">
           {DataFitur.map((fitur) => {
             return (
-              <div  key={fitur.id} style={{background: fitur.warna}} className="lg:col-span-3 md:col-span-4 col-span-6 py-[25px] rounded-lg">
-              <Card href={fitur.slug} hasImage={true} src={fitur.icon} imgStyle="w-[80px] mx-auto">
-                <p className="font-bold mx-auto">{fitur.namaFitur}</p>
-              </Card></div>
+              <div
+                key={fitur.id}
+                style={{ background: fitur.warna }}
+                className="lg:col-span-3 md:col-span-4 col-span-6 py-[25px] rounded-lg"
+              >
+                <Card
+                  href={fitur.slug}
+                  hasImage={true}
+                  src={fitur.icon}
+                  imgStyle="w-[80px] mx-auto"
+                >
+                  <p className="font-bold mx-auto">{fitur.namaFitur}</p>
+                </Card>
+              </div>
             );
           })}
         </div>
