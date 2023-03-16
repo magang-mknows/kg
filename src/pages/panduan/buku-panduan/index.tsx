5f5ad5b54cbdae6cd00e39fb6ce4e579af452733
GarnesPratama <97274527+GarnesPratama@users.noreply.github.com>
2023-03-16 18:05:03 +0700

    improvement : rename GuideBook to RegistrationBook

--- src/pages/panduan/buku-panduan/index.tsx
+++ src/pages/panduan/buku-panduan/index.tsx
@@ -1,9 +1,9 @@
 import BreadCrumb from "@/components/Assigment/BreadCrumb";
 import Loading from "@/components/Loading";
 import BaseLayoutsLanding from "@/layouts/BaseLanding";
-import AnotherGuideBookSection from "@/modules/GuideBook/AnotherGuideBookSection";
-import ContentSection from "@/modules/GuideBook/ContentSection";
-import HeroSection from "@/modules/GuideBook/HeroSection";
+import AnotherGuideBookSection from "@/modules/RegistrationBook/AnotherGuideBookSection";
+import ContentSection from "@/modules/RegistrationBook/ContentSection";
+import HeroSection from "@/modules/RegistrationBook/HeroSection";
 import { guideBookBreadCumbs } from "@/utilities/constant";
 import React, { ReactElement, Suspense } from "react";
 
