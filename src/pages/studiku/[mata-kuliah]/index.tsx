a95f766b3d8cb372e3bbc5ef32f6f5045bf41780
Nandi <nandiirawan327@gmail.com>
2023-03-16 18:10:07 +0700

    feature: add new page 'course'

---
+++ src/pages/studiku/[mata-kuliah]/index.tsx
@@ -0,0 +1,12 @@
+import Course from "@/modules/Studiku/Course";
+import { FC, Fragment, ReactElement } from "react";
+
+const CoursePage: FC = (): ReactElement => {
+  return (
+    <Fragment>
+      <Course />
+    </Fragment>
+  );
+};
+
+export default CoursePage;
