c7572ee992daf9f39703194cb2256e63e0408373
GarnesPratama <97274527+GarnesPratama@users.noreply.github.com>
2023-03-21 16:39:27 +0700

    improvement : add dynamic in guide video

--- src/modules/GuideVideo/ContentSection.tsx
+++ src/modules/GuideVideo/ContentSection.tsx
@@ -3,6 +3,8 @@ import Video from "@/assets/guide/video.svg";
 import Card from "@/components/Common/Card";
 import PlayButton from "@/assets/guide/play-button.svg";
 import Image from "next/image";
+import { useGuideVideo } from "@/hooks/Guide/useVideoGuide";
+import Link from "next/link";
 
 const dummyGuide = [
   {
@@ -44,20 +46,22 @@ const dummyGuide = [
 ];
 
 const ContentSection = (): ReactElement => {
+  const {getGuideVideo} = useGuideVideo();
   return (
     <div className="w-full px-6 md:px-8 lg:px-10">
       <div className="mt-[24px] mb-[54px] justify-between grid lg:grid-cols-3 md:grid-cols-2 lg:gap-x-8 md:gap-x-8 md:gap-y-5 gap-y-5">
-        {dummyGuide.slice(0, 6).map((dummy, i) => {
+        {getGuideVideo.slice(0, 6).map((item, i) => {
           return (
-            <Card
-              key={i}
-              href="/"
+            <Link href={`/video-panduan-pendaftaran/${item.videoId}`}  key={i}>
+              <Card
               className="relative w-full min-h-min border rounded-lg px-3 shadow-md"
-              src={dummy.video}
-              imgStyle="w-full"
+              src={`https://i.ytimg.com/vi/${item.videoId}/hq720.jpg`}
+              imgwidth={380}
+              imgheight={170}
+              imgStyle="w-full rounded-[8px]"
               hasImage={true}
               titleStyle={"text-xl font-bold mt-2"}
-              title={dummy.title}
+              title={item.titleVideo}
               icon={
                 <div className="absolute bottom-[65%] left-[45%]">
                   <Image src={PlayButton} alt="play button" />
@@ -65,9 +69,11 @@ const ContentSection = (): ReactElement => {
               }
             >
               <p className="w-full text-sm font-base mt-3.5" style={{ color: "#000000" }}>
-                {dummy.description.substring(0, 80)}...
+                {item.descVideo?.substring(0,80)}...
               </p>
             </Card>
+            </Link>
+
           );
         })}
       </div>
