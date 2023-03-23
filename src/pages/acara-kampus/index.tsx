9a2d9db13fabcad9eedd4326bf4cb7a72b4948ef
Muhamad Fauzan <47935196+fauzannmw@users.noreply.github.com>
2023-03-23 17:35:48 +0700

    feature: add Popup Discussion

--- src/pages/acara-kampus/index.tsx
+++ src/pages/acara-kampus/index.tsx
@@ -2,7 +2,6 @@ import type { NextPage } from "next";
 import { lazy, ReactElement, Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 import Loading from "@/components/Loading";
-import CreateDiscussion from "@/modules/DiscussionRoom/CreateDiscussion";
 
 const Event = lazy(() => import("@/modules/Event"));
 
@@ -11,7 +10,6 @@ const EventPage: NextPage = (): ReactElement => {
     <ErrorBoundary fallback={<>Error was happen</>}>
       <Suspense fallback={<Loading />}>
         <Event />
-        <CreateDiscussion />
       </Suspense>
     </ErrorBoundary>
   );
