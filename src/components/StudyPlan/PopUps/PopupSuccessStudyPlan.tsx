9f2e119d5ea1625c795c2fc10ec9da125000ed5f
Nandi <nandiirawan327@gmail.com>
2023-03-25 19:50:59 +0700

    feature: add new success popup

--- src/components/StudyPlan/PopUps/PopupSuccessStudyPlan.tsx
+++ src/components/StudyPlan/PopUps/PopupSuccessStudyPlan.tsx
@@ -15,21 +15,22 @@ const PopupSuccessStudyPlan: FC = (): ReactElement => {
       icon={Success}
       popupTitle="Berhasil"
       lookup={popupSuccessStatus}
-      className="!h-80 !w-[100%] text-md py-10"
+      className="!h-80 !w-[100%] text-md"
+      widthModal="!max-w-[590px]"
     >
-      <div className="flex gap-x-[10px] bg-primary-100 rounded-lg p-3">
-        <Image src={InfoBlueIcon} alt="info-blue-icon" />
-        <p className="text-base text-primary-500 font-normal">
+      <div className="flex items-center justify-around gap-x-[10px] bg-primary-100 rounded-lg p-3">
+        <Image src={InfoBlueIcon} alt="info-blue-icon" className="w-5 h-5" />
+        <p className="text-base text-primary-500 font-normal w-[80%] sm:w-auto">
           Hasil konversi bisa dilihat pada tab pengajuan
         </p>
       </div>
-      <h1 className="py-2 text-2xl">
+      <h1 className="py-2 text-xl font-normal text-black">
         Kamu berhasil mengajukan konversi mata kuliah Logika Algoritma
       </h1>
       <div className="w-full mt-6">
         <Button
           text="Tutup"
-          className="w-[60%] h-14 mx-auto rounded-lg bg-primary-500 text-white"
+          className="w-[95%] h-14 mx-auto rounded-lg bg-primary-500 text-white"
           onClick={() => setPopupSuccessStatus(false)}
           type={"button"}
         />
