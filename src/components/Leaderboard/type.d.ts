3cd09a06be8bfdd284680eea5b5bdd1463701851
Noor Yulita Apsari <72492009+lita15@users.noreply.github.com>
2023-03-28 17:20:10 +0700

    improvement: recomponent popup

--- src/components/Leaderboard/type.d.ts
+++ src/components/Leaderboard/type.d.ts
@@ -4,11 +4,11 @@ import { MouseEventHandler, ReactNode } from "react";
 export interface PopupProfilProps {
     name?: string,
     point?: number,
-    popupTitle?: string,
+    popupRank?: number,
     image?: StaticImageData,
     lookup?: boolean,
-    onClose?: boolean,
-    children?: ReactNode,
+    semester?: string,
+    major?: string
     stylePopup?: string,
     widthModal?: string,
     onClose?: MouseEventHandler<HTMLDivElement | HTMLSpanElement>,
--- src/modules/Leaderboard/RankingSection.tsx
+++ src/modules/Leaderboard/RankingSection.tsx
@@ -1,14 +1,12 @@
 import Image from "next/image";
 import React, { FC, ReactElement } from "react";
 import Avatar from "@/assets/leaderboard/avatar.svg";
-import Like from "@/assets/leaderboard/like.svg";
 import { leaderBoardProps } from "./type";
-import PopupModal from "@/components/Common/PopupModal";
-import Modal from "@/components/Common/Modal";
 import PopupProfil from "@/components/Leaderboard/PopupProfil";
 import { usePopupProfilLeaderboard } from "@/hooks/Leaderborad/usePopupProfilLeaderboard";
 const RankingSection: FC<leaderBoardProps> = ({ name, img, score, index }): ReactElement => {
   const { setPopupLeaderboardStatus, getPopupLeaderboardStatus } = usePopupProfilLeaderboard();
+
   return (
     <>
       <div>
@@ -30,48 +28,16 @@ const RankingSection: FC<leaderBoardProps> = ({ name, img, score, index }): Reac
           </div>
         </div>
       </div>
-      <PopupProfil lookup={getPopupLeaderboardStatus} widthModal="!max-w-[900px]">
-        <div>
-          <h1 className="mb-5 text-[#737373] font-[500] text-[18px]">
-            Rank <span className="text-[#106FA4] font-[600] text-[28px]">4</span> Global
-          </h1>
-          <div className="flex flex-row outline justify-between outline-[#E5E5E5] bg-[#FAFAFA] rounded-[8px] px-3 h-[125px] w-[636px] items-center">
-            <div className="flex items-center gap-7">
-              <Image src={Avatar} alt={"avatar"} className="w-24 h-24" />
-              <div className="">
-                <p className="text-[#171717] font-[600] text-[24px]">Edhit Expilen</p>
-                <div className="flex flex-row text-[#737373] text-[20px] font-[500] gap-2">
-                  <p>Blockchains </p>
-                  <p>Semster 5</p>
-                </div>
-              </div>
-            </div>
-            <div className="flex item-center">
-              <button className="bg-[#FAB317] text-white text-[16px] font-[600] rounded-[8px] px-2 py-1">
-                7000 Poin
-              </button>
-            </div>
-          </div>
-          <div className="content w-full mt-5">
-            <div className="flex flex-row w-full justify-between">
-              <div className="px-3 py-3 bg-[#CDF5FA] rounded-[8px] flex flex-row items-center gap-3">
-                <Image src={Like} alt={"like"} />
-                <div className="text-[#106FA4] text-[16px] font-[500]">
-                  <p>Jumlah like yang didapat</p>
-                  <p className="text-[24px]">100</p>
-                </div>
-              </div>
-              <div className="px-3 py-3 bg-[#CDF5FA] rounded-[8px] flex flex-row items-center gap-3">
-                <Image src={Like} alt={"like"} />
-                <div className="text-[#106FA4] text-[16px] font-[500]">
-                  <p>Jumlah like yang didapat</p>
-                  <p className="text-[24px]">100</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </PopupProfil>
+      <PopupProfil
+        lookup={getPopupLeaderboardStatus}
+        widthModal="!max-w-[748px]"
+        popupRank={4}
+        point={7000}
+        image={Avatar}
+        name={"Tanto Sutowo"}
+        major={"Blokchain"}
+        semester={"5"}
+      ></PopupProfil>
     </>
   );
 };
