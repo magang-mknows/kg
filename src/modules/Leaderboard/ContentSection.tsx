80661867e9d364853ccce47308149c7d052519e0
Nandi <nandiirawan327@gmail.com>
2023-03-28 13:34:42 +0700

    merge

--- src/modules/Leaderboard/ContentSection.tsx
+++ src/modules/Leaderboard/ContentSection.tsx
@@ -6,9 +6,7 @@ import Medal from "@/assets/leaderboard/medal.svg";
 import Avatar from "@/assets/leaderboard/avatar.svg";
 import RankingSection from "./RankingSection";
 import Pagination from "../Event/Common/Pagination";
-import Filter from "./Filter";
-
-
+import Filter from "./filter";
 
 const dummyRanking = [
   {
