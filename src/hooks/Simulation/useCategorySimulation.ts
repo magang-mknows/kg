6d8f92e4756bcd18ea1009b25eb3668eb2e9f3cb
Salsabila Dita Prasetya <avocadobanana33@gmail.com>
2023-03-29 15:24:25 +0700

    improvement: change state in popup

--- src/hooks/Simulation/useCategorySimulation.ts
+++ src/hooks/Simulation/useCategorySimulation.ts
@@ -1,9 +1,14 @@
 import { useRecoilState } from "recoil";
-import { categorySimulationState } from "@/stores/Simulation";
+import { categorySimulationState, chooseScheduleState } from "@/stores/Simulation";
 
 type CategorySimulationTypes = {
     setCategorySimulation: (val: string) => void;
     getCategorySimulation: string;
+}
+
+type ScheduleTypes = {
+    setScheduleSimulation: (val: boolean) => void;
+    getScheduleSimulation: boolean;
 }
 
 export const useCategorySimulation = (): CategorySimulationTypes => {
@@ -11,5 +16,13 @@ export const useCategorySimulation = (): CategorySimulationTypes => {
     return{
         setCategorySimulation: (val: string) => setCategory(val),
         getCategorySimulation: getCategory,
+    };
+};
+
+export const useScheduleSimulation = (): ScheduleTypes => {
+    const [getSchedule, setSchedule] = useRecoilState(chooseScheduleState);
+    return{
+        setScheduleSimulation: (val: boolean) => setSchedule(val),
+        getScheduleSimulation: getSchedule,
     };
 };
