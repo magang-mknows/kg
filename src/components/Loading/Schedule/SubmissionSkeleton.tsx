1856dd92d5d81c088a3db0f87076c7d3c7fc8b9d
Noor Yulita Apsari <72492009+lita15@users.noreply.github.com>
2023-03-30 17:04:03 +0700

    improvement: delete folder skeleton simulation

--- src/components/Loading/Schedule/SubmissionSkeleton.tsx
+++
@@ -1,8 +0,0 @@
-import { FC, ReactElement } from "react";
-import Card from "@/components/Common/Card";
-
-const SubmissionSkeleton: FC = (): ReactElement => {
-  return <div></div>;
-};
-
-export default SubmissionSkeleton;
