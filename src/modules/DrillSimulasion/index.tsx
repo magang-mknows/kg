8d5a023e6bdd69f93c09e98b9ab14f84d58e621f
Noor Yulita Apsari <72492009+lita15@users.noreply.github.com>
2023-03-31 01:17:14 +0700

    improvment: add content skeleton submission

--- src/modules/DrillSimulasion/index.tsx
+++ src/modules/DrillSimulasion/index.tsx
@@ -1,64 +1,29 @@
 import BaseLayouts from "@/layouts/Base";
 import { FC, lazy, ReactElement, Suspense } from "react";
 import { drillSimulation } from "@/utilities/constant";
-import { Tab } from "@headlessui/react";
-import SimulationSubmission from "./SimulationSubmission";
-import SimulationHistory from "./SimulationHistory";
 import SuspenseError from "../Common/SuspenseError";
 import TitleSkeleton from "@/components/Loading/Simulasion/TitleSkeleton";
+import SubmissionSkeleton from "@/components/Loading/Simulasion/SubmissionSkeleton";
 
 const Title = lazy(() => import("@/modules/DrillSimulasion/Title"));
+const Content = lazy(() => import("@/modules/DrillSimulasion/Content"));
 const BreadCrumbs = lazy(() => import("@/components/Assigment/BreadCrumb"));
 
 const DrillSimulasion: FC = (): ReactElement => {
   return (
     // <BaseLayouts>
     <>
-      {/* <BreadCrumbs items={drillSimulation} /> */}
+      <BreadCrumbs items={drillSimulation} />
       <div className="px-6 md:px-8 lg:px-10 w-full">
         <SuspenseError loadingFallback={<TitleSkeleton />}>
           <Title />
         </SuspenseError>
-        <Tab.Group>
-          <Tab.List
-            as={"div"}
-            className=" mb-10 border-b-2 flex gap-6 px-2 text-lg text-neutral-400 font-medium"
-          >
-            <Tab>
-              {({ selected }) => (
-                <div
-                  className={`${
-                    selected ? "border-b-2 outline-none  border-primary-500 text-primary-500" : ""
-                  } py-2 px-4 cursor-pointer`}
-                >
-                  Pengajuan Simulasi
-                </div>
-              )}
-            </Tab>
-            <Tab as={"div"}>
-              {({ selected }) => (
-                <div
-                  className={`${
-                    selected ? "border-b-2 outline-none  border-primary-500 text-primary-500" : ""
-                  } py-2 px-4 cursor-pointer`}
-                >
-                  Riwayat Simulasi
-                </div>
-              )}
-            </Tab>
-          </Tab.List>
-          <Tab.Panels>
-            <Tab.Panel>
-              <SimulationSubmission />
-            </Tab.Panel>
-            <Tab.Panel>
-              <SimulationHistory />
-            </Tab.Panel>
-          </Tab.Panels>
-        </Tab.Group>
+        <SuspenseError loadingFallback={<SubmissionSkeleton />}>
+          <Content />
+        </SuspenseError>
       </div>
     </>
-    /* </BaseLayouts> */
+    // </BaseLayouts>
   );
 };
 
--- src/pages/drill-simulasi/index.tsx
+++ src/pages/drill-simulasi/index.tsx
@@ -2,14 +2,28 @@ import { lazy, ReactElement, Suspense } from "react";
 import type { NextPage } from "next";
 import TitleSkeleton from "@/components/Loading/Simulasion/TitleSkeleton";
 import SuspenseError from "@/modules/Common/SuspenseError";
+import SubmissionSkeleton from "@/components/Loading/Simulasion/SubmissionSkeleton";
+import BaseLayouts from "@/layouts/Base";
+import { drillSimulation } from "@/utilities/constant";
 
 const DrillSimulasion = lazy(() => import("@/modules/DrillSimulasion"));
+const Title = lazy(() => import("@/modules/DrillSimulasion/Title"));
+const Content = lazy(() => import("@/modules/DrillSimulasion/Content"));
+const BreadCrumbs = lazy(() => import("@/components/Assigment/BreadCrumb"));
 
 const drillSimulasion: NextPage = (): ReactElement => {
   return (
-    <SuspenseError loadingFallback={<TitleSkeleton />}>
-      <DrillSimulasion />
-    </SuspenseError>
+    <BaseLayouts>
+      {/* <BreadCrumbs items={drillSimulation} /> */}
+      <div className="px-6 md:px-8 lg:px-10 w-full">
+        <SuspenseError loadingFallback={<TitleSkeleton />}>
+          <Title />
+        </SuspenseError>
+        <SuspenseError loadingFallback={<SubmissionSkeleton />}>
+          <Content />
+        </SuspenseError>
+      </div>
+    </BaseLayouts>
   );
 };
 
