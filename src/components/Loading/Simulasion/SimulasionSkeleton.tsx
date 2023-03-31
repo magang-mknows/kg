a523fd5957c36a46aa84f6d51f2702eaf9f068b6
Noor Yulita Apsari <72492009+lita15@users.noreply.github.com>
2023-03-31 17:14:29 +0700

    improvement: add breadcrumb skeleton in simualtion page

--- src/components/Loading/Simulasion/SimulasionSkeleton.tsx
+++ src/components/Loading/Simulasion/SimulasionSkeleton.tsx
@@ -1,10 +1,12 @@
 import { FC, ReactElement } from "react";
 import TitleSkeleton from "./TitleSkeleton";
 import SubmissionSkeleton from "./SubmissionSkeleton";
+import BreadCrumbSkeleton from "@/components/Loading/Assigment/Breadcrumb";
 
 const SimulasionSkeleton: FC = (): ReactElement => {
   return (
-    <div className="mt-56">
+    <div className="mt-36">
+      <BreadCrumbSkeleton />
       <TitleSkeleton />
       <SubmissionSkeleton />
     </div>
