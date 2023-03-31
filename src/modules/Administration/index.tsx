83bd4c6b2a0a94dd242dbef1f461a1abc9e09317
Noor Yulita Apsari <72492009+lita15@users.noreply.github.com>
2023-03-31 17:44:36 +0700

    improvement: change suspense error in administration pages

--- src/modules/Administration/index.tsx
+++ src/modules/Administration/index.tsx
@@ -2,23 +2,23 @@ import BaseLayouts from "@/layouts/Base";
 import { FC, lazy, ReactElement } from "react";
 import { administrationBreadCumbs } from "@/utilities/constant";
 import BreadCrumb from "@/components/Assigment/BreadCrumb";
-import TitleAdministration from "./TitleAdministration";
-import AdministrationSkeleton from "@/components/Loading/Administration/AdministrationSkeleton";
+import AdministrationSkeleton from "@/components/Loading/Administration/ContentSkeleton";
 import SuspenseError from "@/modules/Common/SuspenseError";
 
 const ContentAdministration = lazy(() => import("@/modules/Administration/ContentAdministration"));
+const TitleAdministration = lazy(() => import("@/modules/Administration/TitleAdministration"));
 
 const Administration: FC = (): ReactElement => {
   return (
-    <BaseLayouts>
-      <div className=" bg-[#F5F5F5] w-full">
-        <SuspenseError loadingFallback={<AdministrationSkeleton />}>
+    <SuspenseError loadingFallback={<AdministrationSkeleton />}>
+      <BaseLayouts>
+        <div className=" bg-[#F5F5F5] w-full">
           <BreadCrumb items={administrationBreadCumbs} />
           <TitleAdministration />
           <ContentAdministration />
-        </SuspenseError>
-      </div>
-    </BaseLayouts>
+        </div>
+      </BaseLayouts>
+    </SuspenseError>
   );
 };
 
--- src/pages/administrasi/index.tsx
+++ src/pages/administrasi/index.tsx
@@ -1,14 +1,11 @@
 import type { NextPage } from "next";
 import { lazy, ReactElement, Suspense } from "react";
-import { ErrorBoundary } from "react-error-boundary";
-import { useQueryErrorResetBoundary } from "@tanstack/react-query";
-import AdministrationSkeleton from "@/components/Loading/Administration/AdministrationSkeleton";
+import AdministrationSkeleton from "@/components/Loading/Administration/ContentSkeleton";
 import SuspenseError from "@/modules/Common/SuspenseError";
 
 const Administration = lazy(() => import("@/modules/Administration"));
 
 const AdministrationPages: NextPage = (): ReactElement => {
-  const { reset } = useQueryErrorResetBoundary();
   return (
     <SuspenseError loadingFallback={<AdministrationSkeleton />}>
       <Administration />
