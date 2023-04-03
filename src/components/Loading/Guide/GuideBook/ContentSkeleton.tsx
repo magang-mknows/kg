9ce0c7464df3c8ec1fc50d91ab6ae906f6d4b29e
Noor Yulita Apsari <72492009+lita15@users.noreply.github.com>
2023-04-03 16:55:05 +0700

    improvement: add array in in skeleton schedule

--- src/components/Loading/Guide/GuideBook/ContentSkeleton.tsx
+++ src/components/Loading/Guide/GuideBook/ContentSkeleton.tsx
@@ -7,7 +7,7 @@ const ContentSkeleton: FC = (): ReactElement => {
         <h1 className="animate-pulse bg-neutral-200 w-64 py-3 rounded-lg"></h1>
       </div>
       <div className="mt-[24px] mb-[54px]">
-        {[...Array(3)].map((_, i) => (
+        {[...Array(2)].map((_, i) => (
           <div
             key={i}
             className="animate-pulse bg-neutral-200 rounded-lg w-full h-[54px] shadow-md  flex justify-between items-center p-[24px] mt-[16px]"
--- src/components/Loading/Simulasion/SubmissionSkeleton.tsx
+++ src/components/Loading/Simulasion/SubmissionSkeleton.tsx
@@ -9,24 +9,30 @@ const SubmissionSkeleton: FC = (): ReactElement => {
           <div className="animate-pulse bg-neutral-200 w-48 py-4 rounded-lg "></div>
         </div>
         <div className="animate-pulse bg-neutral-100 py-1 w-11/12 rounded-lg mt-4 flex ml-10"></div>
-
-        <div className="flex flex-col mb-20 lg:justify-start md:justify-around justify-center md:gap-10 lg:gap-20 gap-5 mt-10 ml-10 animate-pulse bg-neutral-100 w-[381px] rounded-lg ">
-          <div className="mb-10 px-5 w-full">
-            <div className="animate-pulse bg-neutral-300 w-[381px] py-24 rounded-lg -ml-5 relative z-10">
-              <div
-                className="animate-pulse bg-neutral-100 w-20 py-3 rounded-lg px-3 absolute z-20 top-8 right-6 justify-end
+        <div className="flex flex-row gap-10">
+          {[...Array(3)].map((_, i) => (
+            <div
+              key={i}
+              className="flex flex-col mb-20 lg:justify-start md:justify-around justify-center md:gap-10 lg:gap-20 gap-5 mt-10 ml-10 animate-pulse bg-neutral-100 w-[381px] rounded-lg "
+            >
+              <div className="mb-10 px-5 w-full">
+                <div className="animate-pulse bg-neutral-300 w-[381px] py-24 rounded-lg -ml-5 relative z-10">
+                  <div
+                    className="animate-pulse bg-neutral-100 w-20 py-3 rounded-lg px-3 absolute z-20 top-8 right-6 justify-end
                 "
-              ></div>
+                  ></div>
+                </div>
+                <div className="flex gap-4 mt-5">
+                  <div className="animate-pulse bg-neutral-300 py-3 w-36 rounded-lg"></div>
+                  <div className="animate-pulse bg-neutral-300 py-3 w-24 rounded-lg"></div>
+                </div>
+                <div className="animate-pulse bg-neutral-300 py-3 w-56 rounded-lg mt-5"></div>
+                <div className="flex mt-8 justify-end">
+                  <div className="animate-pulse bg-neutral-300 w-[182px] h-[35px] rounded-lg" />
+                </div>
+              </div>
             </div>
-            <div className="flex gap-4 mt-5">
-              <div className="animate-pulse bg-neutral-300 py-3 w-36 rounded-lg"></div>
-              <div className="animate-pulse bg-neutral-300 py-3 w-24 rounded-lg"></div>
-            </div>
-            <div className="animate-pulse bg-neutral-300 py-3 w-56 rounded-lg mt-5"></div>
-            <div className="flex mt-8 justify-end">
-              <div className="animate-pulse bg-neutral-300 w-[182px] h-[35px] rounded-lg" />
-            </div>
-          </div>
+          ))}
         </div>
       </section>
     </>
