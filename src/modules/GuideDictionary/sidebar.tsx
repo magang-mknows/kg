ea3260ef4bda1876e4f6067d0e0a924810024592
Noor Yulita Apsari <72492009+lita15@users.noreply.github.com>
2023-04-04 20:43:56 +0700

    improvement: create kamus panduan skeleton

--- src/modules/GuideDictionary/sidebar.tsx
+++ src/modules/GuideDictionary/sidebar.tsx
@@ -5,7 +5,7 @@ import { useGuideVideo } from "@/hooks/Guide/useGuideDictionary";
 const Sidebar = (): ReactElement => {
   const { getGuideDictionary } = useGuideVideo();
   return (
-    <div className="px-6 md:px-8 lg:px10 mb-10">
+    <div className="px-6 md:px-8 lg:px-10 mb-10">
       <h1 className=" text-[20px] font-[700] mb-2">Kamus Populer </h1>
       <div className="grid grid-cols-2 lg:grid-cols-1 md:grid-cols-3 gap-2 lg:gap-0 md:gap-4">
         {getGuideDictionary.map((dummy, i) => {
