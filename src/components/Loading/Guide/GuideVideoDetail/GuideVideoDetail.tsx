793ab2fa4944a587c5f88564c146994a9fa0ece8
Noor Yulita Apsari <72492009+lita15@users.noreply.github.com>
2023-04-04 21:38:38 +0700

    improvement: delete comment

--- src/components/Loading/Guide/GuideVideoDetail/GuideVideoDetail.tsx
+++ src/components/Loading/Guide/GuideVideoDetail/GuideVideoDetail.tsx
@@ -8,13 +8,9 @@ const GuideVideoDetail: FC = (): ReactElement => {
     <div className="mt-36">
       <BreadCrumbDetilV />
       <div className="w-full px-6 md:px-8 lg:px10 my-[68px]">
-        <div className="flex-row lg:flex gap-[68px] xl:flex-nowrap lg:flex-wrap">
+        <div className="flex-rowlg:flex gap-[40px] xl:flex-nowrap lg:flex-wrap">
           <YoutubeSkeleton />
           <DescriptionSkeleton />
-
-          {/* <div className="w-full">
-          <ListVideo />
-        </div> */}
         </div>
       </div>
     </div>
