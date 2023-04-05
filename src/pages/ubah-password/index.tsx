a52aa7a88aa921ee25257a2b13bac46cfefab5bf
Salsabila Dita Prasetya <avocadobanana33@gmail.com>
2023-04-05 17:11:04 +0700

    improvement: add profil skeleton in resetPassword

--- src/pages/ubah-password/index.tsx
+++ src/pages/ubah-password/index.tsx
@@ -2,15 +2,17 @@ import type { NextPage } from "next";
 import { lazy, ReactElement, Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 import Loading from "@/components/Loading";
+import SuspenseError from "@/modules/Common/SuspenseError";
+import ProfilSkeleton from "@/components/Loading/Profil/ProfilSkeleton";
 
 const ResetPassword = lazy(() => import("@/modules/Profile/ResetPasswordSection"));
 
 const ResetPasswordPage: NextPage = (): ReactElement => {
   return (
     <ErrorBoundary fallback={<>Error was happen</>}>
-      <Suspense fallback={<Loading />}>
+      <SuspenseError loadingFallback={<ProfilSkeleton />}>
         <ResetPassword />
-      </Suspense>
+      </SuspenseError>
     </ErrorBoundary>
   );
 };
