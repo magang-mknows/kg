154ae95e16a6275f2601328e348adc2974dfcf36
Noor Yulita Apsari <72492009+lita15@users.noreply.github.com>
2023-04-05 15:51:54 +0700

    improvement: responsive xs leaderboard

--- src/modules/Leaderboard/filter.tsx
+++ src/modules/Leaderboard/filter.tsx
@@ -1,5 +1,4 @@
 import React, { ReactElement, useState } from "react";
-import { RxSlider } from "react-icons/rx";
 import moduleFour from "@/assets/leaderboard/module-four.svg";
 import Image from "next/image";
 
@@ -8,12 +7,12 @@ const Filter = (): ReactElement => {
   const [filter, setFilter] = useState("global");
   return (
     <button
-      className="w-[109px] h-[40px] rounded-[8px] text-center border-[3px] border-[#106FA4] relative mt-[46px] "
+      className="md:w-[109px] md:h-[40px] w-20 h-8 rounded-[8px] text-center border-[3px] border-[#106FA4] relative mt-[46px] "
       onClick={() => setOpen(open == true ? false : true)}
     >
-      <div className="my-auto flex justify-center gap-[10px] items-center">
-        <Image src={moduleFour} alt={"image"} />
-        <p className="text-[#106FA4] text-[14px] dark:text-white">Rules</p>
+      <div className="my-auto flex justify-center md:gap-[10px] gap-[4px] items-center">
+        <Image src={moduleFour} alt={"image"} className="md:w-[30px] w-[20px]" />
+        <p className="text-[#106FA4] md:text-[14px] text-[10px] dark:text-white">Rules</p>
       </div>
       {open && (
         <div className="flex justify-end container mt-[10px] z-[9000] absolute w-[150px] -bottom-40 ease-in-out ">
@@ -27,7 +26,7 @@ const Filter = (): ReactElement => {
                   filter == "global" ? "bg-[#67A5C8]" : "bg-[#FFFF]"
                 } absolute left-0 overflow-hidden hover:hover:bg-[#F5F5F5]`}
               ></div>
-              <p className="text-[#262626] text-[14px] my-auto ml-3">Global</p>
+              <p className="text-[#262626] md:text-[14px] text-[10px] my-auto ml-3">Global</p>
             </button>
             <button
               className="flex justify-start h-[38px] relative hover:bg-[#F5F5F5] w-full bg-white "
@@ -38,7 +37,7 @@ const Filter = (): ReactElement => {
                   filter == "fakultas" ? "bg-[#67A5C8]" : "bg-[#FFFF]"
                 } absolute left-0 overflow-hidden hover:hover:bg-[#F5F5F5]`}
               ></div>
-              <p className="text-[#262626] text-[14px] my-auto ml-3">Fakultas</p>
+              <p className="text-[#262626] md:text-[14px] text-[10px] my-auto ml-3">Fakultas</p>
             </button>
             <button
               className="flex justify-start h-[38px] relative hover:bg-[#F5F5F5] w-full bg-white "
@@ -49,7 +48,7 @@ const Filter = (): ReactElement => {
                   filter == "prodi" ? "bg-[#67A5C8]" : "bg-[#FFFF]"
                 } absolute left-0 overflow-hidden`}
               ></div>
-              <p className="text-[#262626] text-[14px] my-auto ml-3">Prodi</p>
+              <p className="text-[#262626] md:text-[14px] text-[10px] my-auto ml-3">Prodi</p>
             </button>
             <button
               className="flex justify-start h-[38px] relative hover:bg-[#F5F5F5] w-full bg-white "
@@ -60,7 +59,7 @@ const Filter = (): ReactElement => {
                   filter == "semester" ? "bg-[#67A5C8]" : "bg-[#FFFF]"
                 } absolute left-0 overflow-hidden`}
               ></div>
-              <p className="text-[#262626] text-[14px] my-auto ml-3">Semester</p>
+              <p className="text-[#262626] md:text-[14px] text-[10px] my-auto ml-3">Semester</p>
             </button>
           </div>
         </div>
