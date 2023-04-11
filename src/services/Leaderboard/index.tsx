2ac9f9e78b10a85d4d91568abff816c31e0e3208
Noor Yulita Apsari <72492009+lita15@users.noreply.github.com>
2023-04-11 15:00:07 +0700

    improvement: get all leaderboard

--- src/services/Leaderboard/index.tsx
+++ src/services/Leaderboard/index.tsx
@@ -1,17 +1,21 @@
 import { handleError } from "@/utilities/helper";
 import ApiService from "../Api";
-
+import TokenService from "../Token";
+const token = TokenService.getToken();
 const LeaderboardService = {
   GetAllLeaderboard: async () => {
     const requestData = {
       method: "GET",
+
       headers: {
         "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
       },
       url: "/leaderboard",
     };
     try {
       const res = await ApiService.customRequest(requestData);
+
       return res.data;
     } catch (error) {
       throw handleError(error);
