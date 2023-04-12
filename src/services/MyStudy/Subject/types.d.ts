412f727ab3a5665e38ced71f0debdb375e96cd79
Salsabila Dita Prasetya <avocadobanana33@gmail.com>
2023-04-12 15:22:57 +0700

    improvement: add matkul api

---
+++ src/services/MyStudy/Subject/types.d.ts
@@ -0,0 +1,26 @@
+import { TMeta } from "@/services/types";
+
+export type TSubject = {
+    id: string,
+    name: string,
+    subject_code: string,
+    degree: string,
+    level: number,
+    teacher_id: string,
+    indicator: string,
+    study_experience: string,
+    teaching_materials: string,
+    basic_competencies: string,
+    tools_needed: string,
+    scoring: string,
+    description: string,
+    thumbnail: string,
+    credit: number,
+    slug: string,
+    subject_id: string
+};
+
+export type TSubjectResponse = TMeta<TSubject>;
+
+
+  
