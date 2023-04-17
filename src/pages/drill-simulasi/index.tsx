53a35a34366f04d2ce5a09801608800b47b81d26
Noor Yulita Apsari <72492009+lita15@users.noreply.github.com>
2023-04-17 17:26:10 +0700

    improvement: responsive skeleton simulation

--- src/pages/drill-simulasi/index.tsx
+++ src/pages/drill-simulasi/index.tsx
@@ -8,6 +8,7 @@ const DrillSimulasion = lazy(() => import("@/modules/DrillSimulasion"));
 const drillSimulasion: NextPage = (): ReactElement => {
   return (
     <SuspenseError loadingFallback={<SimulasionSkeleton />}>
+      <SimulasionSkeleton />
       <DrillSimulasion />
     </SuspenseError>
   );
