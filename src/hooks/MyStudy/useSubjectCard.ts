85bd4eca710563483d8824e7f568626fc2f44347
Nandi <nandiirawan327@gmail.com>
2023-04-18 13:45:44 +0700

    improvement: change directory address

--- src/hooks/MyStudy/useSubjectCard.ts
+++ src/hooks/MyStudy/useSubjectCard.ts
@@ -1,5 +1,5 @@
 import SubjectService from "@/services/MyStudy/Subject";
-import { TSubjectResponse } from "@/services/MyStudy/Subject/types";
+import { TSubjectResponse } from "@/modules/MyStudy/type";
 import { UseQueryResult, useQuery } from "@tanstack/react-query";
 
 export const useGetAllSubject = (): UseQueryResult<TSubjectResponse, unknown> =>
