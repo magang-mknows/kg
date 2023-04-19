50ab3a6c295f6795c22fa8e33a2ac113c9541ae5
Noor Yulita Apsari <72492009+lita15@users.noreply.github.com>
2023-04-19 10:43:22 +0700

    imrpovement: recomponent loading skeleton reschedule

--- src/modules/RescheduleSimulation/index.tsx
+++ src/modules/RescheduleSimulation/index.tsx
@@ -3,13 +3,13 @@ import { FC, lazy, ReactElement } from "react";
 import BreadCrumb from "@/components/Assigment/BreadCrumb";
 import { rescheduleSimulationBreadCumbs } from "@/utilities/constant";
 import SuspenseError from "../Common/SuspenseError";
-import RescheduleSkeleton from "@/components/Loading/Reshedule/RescheduleSkeleton";
+import LoadingReschedule from "./loading";
 
 const Content = lazy(() => import("@/modules/RescheduleSimulation/content"));
 
 const RescheduleSimulation: FC = (): ReactElement => {
   return (
-    <SuspenseError loadingFallback={<RescheduleSkeleton />}>
+    <SuspenseError loadingFallback={<LoadingReschedule />}>
       <BaseLayouts className="w-full">
         <BreadCrumb items={rescheduleSimulationBreadCumbs} />
         <Content />
