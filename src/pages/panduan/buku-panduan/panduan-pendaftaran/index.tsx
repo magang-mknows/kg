2a470db52a59a189f140a5eab7834a2505b48d0e
Noor Yulita Apsari <72492009+lita15@users.noreply.github.com>
2023-04-21 02:55:38 +0700

    recomponent: recomponent registration guide page

--- src/pages/panduan/buku-panduan/panduan-pendaftaran/index.tsx
+++ src/pages/panduan/buku-panduan/panduan-pendaftaran/index.tsx
@@ -1,12 +1,12 @@
 import SuspenseError from "@/modules/Common/SuspenseError";
 import { lazy, ReactElement } from "react";
-import GuideRegist from "@/components/Loading/Guide/GuideRegist/GuideRegist";
+import LoadingRegistrationGuide from "@/modules/RegistrationGuide/loading";
 
 const RegistrationGuideModules = lazy(() => import("@/modules/RegistrationGuide/index"));
 
 const RegistrationGuidePages = (): ReactElement => {
   return (
-    <SuspenseError loadingFallback={<GuideRegist />}>
+    <SuspenseError loadingFallback={<LoadingRegistrationGuide />}>
       <RegistrationGuideModules />
     </SuspenseError>
   );
